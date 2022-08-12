b75ccad25e898997161301bf179228a397c2816e
Murgesh Ekunde <mekunde@gmail.com>
2022-08-12 20:24:59 +0530

    Update task4.js

--- task4.js
+++ task4.js
@@ -3,6 +3,10 @@
 var obj1 = { name: "Person 1", age:5 };
 var obj2 = { age :5 , name:"Person 1"};
 
+//fetch properties
+let prop1 = Object.getOwnPropertyNames(obj1);
+let prop2 = Object.getOwnPropertyNames(obj2);
+
 //will loop in for loop, iterating the list of properties and then comparing their values
 for (let i = 0; i <obj1.length; i++) {
     let obj = obj1[i];
