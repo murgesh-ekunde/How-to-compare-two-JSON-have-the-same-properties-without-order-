9066dfba28f09c3c5eac5af871b1052c77a63b67
Murgesh Ekunde <mekunde@gmail.com>
2022-08-12 20:12:37 +0530

    Add files via upload

---
+++ task4.js
@@ -0,0 +1,14 @@
+
+
+var obj1 = { name: "Person 1", age:5 };
+var obj2 = { age :5 , name:"Person 1"};
+
+//will loop in for loop, iterating the list of properties and then comparing their values
+for (let i = 0; i <obj1.length; i++) {
+    let obj = obj1[i];
+    
+    if (obj1[prop] !== obj2[prop]) {
+        console.log("false");
+    }
+  }
+  console.log("true");
